2c2e0bb1d3496f47b70c1a62d78347418a24b1da
Ryan <kabirraiyan23@gmail.com>
2023-08-20 04:32:36 +0600

    msg

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 //import "./style/styles.scss";
 
 //document.querySelector("#app").innerHTML = ``;
+
+// Smooth animation for dropdown
+document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.querySelector('.sub-menu').style.opacity = 1;
+      item.querySelector('.sub-menu').style.pointerEvents = 'auto';
+    });
+    item.addEventListener('mouseout', () => {
+      item.querySelector('.sub-menu').style.opacity = 0;
+      item.querySelector('.sub-menu').style.pointerEvents = 'none';
+    });
+  });
+  
