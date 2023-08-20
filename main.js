35aa49f74faf81be17390cf11088a6ff38a076c5
Ryan <kabirraiyan23@gmail.com>
2023-08-20 23:21:47 +0600

    msg

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
-//import "./style/styles.scss";
 
-//document.querySelector("#app").innerHTML = ``;
-
-// Smooth animation for dropdown
 document.querySelectorAll('.nav-item').forEach(item => {
-    item.addEventListener('mouseover', () => {
-      item.querySelector('.sub-menu').style.opacity = 1;
-      item.querySelector('.sub-menu').style.pointerEvents = 'auto';
-    });
-    item.addEventListener('mouseout', () => {
-      item.querySelector('.sub-menu').style.opacity = 0;
-      item.querySelector('.sub-menu').style.pointerEvents = 'none';
-    });
+  item.addEventListener('click', () => {
+      const subMenu = item.querySelector('.sub-menu');
+      subMenu.style.opacity = 1;
+      subMenu.style.pointerEvents = 'auto';
   });
+});
+
+const searchContainer = document.querySelector('.search-container');
+const searchInput = document.querySelector('.search-input');
+const searchIcon = document.querySelector('.search-icon');
+const navbanner = document.querySelector('.navbanner');
+const clearIcon = document.querySelector('.clear-icon');
+
+searchIcon.addEventListener('click', () => {
+  searchContainer.classList.toggle('active');
   
+  if (searchContainer.classList.contains('active')) {
+    searchInput.style.width = '100%'; 
+    searchInput.focus();
+  } else {
+    searchInput.style.width = '0';
+  }
+});
+
+
+clearIcon.addEventListener('click', () => {
+  searchContainer.classList.remove('active');
+  searchInput.style.width = '0';
+  searchInput.value = ''; 
+});
+
+
+searchInput.addEventListener('input', () => {
+  if (searchInput.value.trim() === '') {
+    clearIcon.style.display = 'none';
+  } else {
+    clearIcon.style.display = 'block';
+  }
+});
